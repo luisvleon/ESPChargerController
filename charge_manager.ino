b101fc932d3f7e563d511c93a81e4981969b9b56
Luis Leon <support@websoftec.net>
2022-06-04 20:00:48 -0400

    All Webpages code moved to webpage.h file

--- charge_manager.ino
+++ charge_manager.ino
@@ -6,16 +6,8 @@
 #include "webpage.h"
 #include <ArduinoJson.h>
 #include "time.h"
-//-----------------------------------------------
-
-// OTA
-//#include <WiFi.h>
-//#include <WiFiClient.h>
-//#include <WebServer.h>
 #include <ESPmDNS.h>
 #include <Update.h>
-
-
 //-----------------------------------------------
 const char* host = "esp32";
 const char* ssid = "ATT4CmRyNa";
@@ -38,7 +30,7 @@ int haltTemp = 50;
 int safeTemp = 30;
 int tempLoops = 0;
 int restartLoops = 0;
-
+String JSONtxt;
 bool offline = false;
 
 //How many loops until refresh params
@@ -48,100 +40,9 @@ int actual_loop = refresh_loops; //first set at refresh_loop value to inmediate 
 //-----------------------------------------------
 WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
-//-----------------------------------------------
-String JSONtxt;
-//-----------------------------------------------
-void handleRoot()
-{
-  server.send(200,"text/html", webpageCont);
-}
 DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 //====================================================================
-
 WebServer serverOTA(82);
-/*
- * Login page
- */
-const char* loginIndex = 
- "<form name='loginForm'>"
-    "<table width='20%' bgcolor='A09F9F' align='center'>"
-        "<tr>"
-            "<td colspan=2>"
-                "<center><font size=4><b>Solar Charge controller Login Page</b></font></center>"
-                "<br>"
-            "</td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<td>Username:</td>"
-        "<td><input type='text' size=25 name='userid'><br></td>"
-        "</tr>"
-        "<br>"
-        "<br>"
-        "<tr>"
-            "<td>Password:</td>"
-            "<td><input type='Password' size=25 name='pwd'><br></td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<tr>"
-            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-        "</tr>"
-    "</table>"
-"</form>"
-"<script>"
-    "function check(form)"
-    "{"
-    "if(form.userid.value=='logan' && form.pwd.value=='targus25')"
-    "{"
-    "window.open('/serverIndex')"
-    "}"
-    "else"
-    "{"
-    " alert('Error Password or Username')/*displays error message*/"
-    "}"
-    "}"
-"</script>"; 
-/*
- * Server Index Page
- */
-const char* serverIndex = 
-"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
-"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
-   "<input type='file' name='update'>"
-        "<input type='submit' value='Update'>"
-    "</form>"
- "<div id='prg'>progress: 0%</div>"
- "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('success!')" 
- "},"
- "error: function (a, b, c) {"
- "}"
- "});"
- "});"
- "</script>"; 
-
 
 
 void setup()
@@ -266,6 +167,11 @@ void loop()
         Serial.println("Charge standby!");
       }      
   }
+}
+
+void handleRoot()
+{
+  server.send(200,"text/html", webpageCont);
 }
 
 bool chargeGo()
